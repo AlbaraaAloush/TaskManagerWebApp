7782ff9a1b705d9c402655ed4f8dcd7fe624a15c
Albaraa <albaraaaloush@gmail.com>
2025-12-14 15:17:40 +0300

    Add project files.

---
+++ TaskManagerWebApp/Models/TaskItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
